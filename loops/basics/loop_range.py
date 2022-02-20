b909823d5d8dd80c5e5222f617924c0354f6a963
ssmerchant89 <ssmerchant89@gmail.com>
2022-02-19 22:30:40 -0500

    added basic examples of loops

---
+++ loops/basics/loop_range.py
@@ -0,0 +1,7 @@
+#1 is the start point (1 is printed)
+#11 is the stop point (11 is NOT printed) 
+for i in range(1,11):
+	print(i)
+
+
+
---
+++ loops/basics/loop_range_reversed.py
@@ -0,0 +1,5 @@
+#11 is the start point (10 is printed)
+#-1 is the stop point (-1 is NOT printed)
+#-1 is the step amount
+for i in range(10, -1, -1):
+	print(i)
